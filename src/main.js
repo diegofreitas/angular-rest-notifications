a348038c54afaa99dd00a986dfaf03b5e5cc3545
Diego Freitas <diego.lins.freitas@gmail.com>
2016-08-30 21:39:30 -0400

    impl fire success

--- src/main.js
+++ src/main.js
@@ -3,11 +3,41 @@
 
 	angular
 		.module('rest-notificatons',[])
+		.provider('restNotification', _provider)
 		.config(config);
+	
+	function RestNotfication(url, rootScope) {
+		
+		var _this = this;
+		_this.url = url;
+		_this.rootScope = rootScope;
+		
+		this.fireSuccess = function(response) {
+			if (response.config.method === 'PUT'
+				|| response.config.method === 'POST'
+				&& response.stutus === 200 && response.url.indexOf(_this.url) >= 0) {
+				_this.rootScope.$broadcast("success",
+					"Operação realizada com sucesso");
+			}
+		}
+		
+	}
+		
+	function _provider() {
+		  var path = "resources";
 
+		  this.setPath = function(restResourcePath) {
+			  path = restResourcePath;
+		  };
+
+		  this.$get = ["$rootScope", function unicornLauncherFactory($rootScope) {
+			  return new RestNotfication(path, $rootScope);
+		  }];
+		});
+	
 	/** @ngInject */
 	function config($httpProvider) {
-		$httpProvider.interceptors.push(function($rootScope, $q, $timeout) {
+		$httpProvider.interceptors.push(function($rootScope, $q, $timeout, RestNotfication) {
 
 			return {
 				'request' : function(config) {
@@ -26,12 +56,7 @@
 					$timeout(function() {
 						$rootScope.loadingProgress = false;
 					});
-					if (response.config.method === 'PUT'
-							|| response.config.method === 'POST'
-							&& response.stutus === 200) {
-						$rootScope.$broadcast("success",
-								"Operação realizada com sucesso");
-					}
+					RestNotfication.fireSuccess(response);
 					return response;
 				}
 			};
