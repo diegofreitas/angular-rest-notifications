e3f59f7de12386494c836098ef4743def5a4e74b
Diego Freitas <diego.lins.freitas@gmail.com>
2016-08-30 20:57:10 -0400

    add http configuration

---
+++ src/main.js
@@ -0,0 +1,41 @@
+(function() {
+	'use strict';
+
+	angular
+		.module('rest-notificatons')
+		.config(config);
+
+	/** @ngInject */
+	function config($httpProvider) {
+		$httpProvider.interceptors.push(function($rootScope, $q, $timeout) {
+
+			return {
+				'request' : function(config) {
+					$rootScope.loadingProgress = true;
+					$rootScope.$broadcast("clear");
+					return config;
+				},
+				'responseError' : function(rejection) {
+					if (rejection.error) {
+						$rootScope.$broadcast("error", rejection.error);
+					}
+
+					return $q.reject(rejection);
+				},
+				'response' : function(response) {
+					$timeout(function() {
+						$rootScope.loadingProgress = false;
+					});
+					if (response.config.method === 'PUT'
+							|| response.config.method === 'POST'
+							&& response.stutus === 200) {
+						$rootScope.$broadcast("success",
+								"Operação realizada com sucesso");
+					}
+					return response;
+				}
+			};
+		});
+	}
+
+})();
